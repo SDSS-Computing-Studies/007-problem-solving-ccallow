1982d20f87e7d49e355905b9c5660228a921c8a6
ccallow <71406811+ccallow@users.noreply.github.com>
2020-10-21 11:42:49 -0700

    l

--- Assignment.py
+++ Assignment.py
@@ -1,7 +1,9 @@
 #!python3
-
 import pyautogui as p
 import time as t
+
+curTime = t.time()
+target = curTime+30
 
 #p.mouseInfo()
 
@@ -9,12 +11,13 @@ cookie = p.locateCenterOnScreen('cooke.png')
 
 print(cookie)
 
-store = p.locateCenterOnScreen('store buttom.png')
+store = p.locateCenterOnScreen('store.png', confidence=0.75)
 print(store)
 
 #1. click the cookie as fast as possible
 while True:
     p.click(cookie) 
+    break
 #2. cycle through upgrades from buttom to top every minute
 
 #3. check the store button every minute
