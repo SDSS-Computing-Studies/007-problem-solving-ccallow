30489397bd16a11743b0737ebf83a9fe6078c778
ccallow <71406811+ccallow@users.noreply.github.com>
2020-10-21 12:41:54 -0700

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -4,9 +4,10 @@ import time as t
 
 curTime = t.time()
 
-p.mouseInfo()
+#p.mouseInfo()
 
-
+store = p.locateCenterOnScreen('store.png', confidence=0.75)
+print(store)
 
 
 #1. click the cookie
@@ -19,15 +20,30 @@ def cookieClicks():
         break
 #2.cycle through upgrades from buttom to top every minute
 def upgrades():
-    store = p.locateCenterOnScreen('store.png', confidence=0.75)
-    print(store)
+    p.moveTo(1193,340)
+    if p.pixelMatchesColour(1193,340(255,255,255)):
+        for i in range(0,5):
+            p.Click(1193,340)
 
-#Cursor (1249,329)
-#Grandma (1249, 389)
-#Farm (1249,449)
-#Mine(1249, 519)
-#Factory (1249, 589)
-#Bank (1249, 659)
-#Temple (1249, 709)
+
+#Cursor (1193, 326)
+#Grandma (1194,393)
+#Farm (1195,454)
+#Mine(1201, 519)
+#Factory (1196,582)
+#Bank (1197,663)
+#Temple (1201,721)
+#white (#FFFFFF)
 
 #3.check the store button every minute
+#leftupgrade(1150,215)
+#rightupgrade(1391,221)
+#blueLight(#0F2835)
+#blueDark(#050F14)
+
+def playGame():
+    cookieClicks()
+    upgrades()
+
+
+playGame()
